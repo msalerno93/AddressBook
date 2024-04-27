36312d65c3f84627b0ffb2010ba3f504d3f79472
Michael Salerno <msalerno1993@gmail.com>
2024-04-27 19:35:21 -0400

    added packages

--- server/server.js
+++ server/server.js
@@ -1,9 +1,3 @@
-const http = require('http');
+const {v4: uuidv4} = require('uuid')
 
-
-const server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
-}).listen(8081);
-
-console.log('Server running at http://127.0.0.1:8081/');
+console.log(uuidv4());
