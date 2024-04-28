2bff34d1f3b37216c816c02719d2e932a029e7d8
Michael Salerno <msalerno1993@gmail.com>
2024-04-27 20:33:06 -0400

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+dotenv.config()
+mongoose.set("strictQuery", false);
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT;
+const connectionString = process.env.MONGOOSE_URL
 
 const people = [
   { name: "John", industry: "Tech" },
@@ -8,7 +15,32 @@ const people = [
   { name: "Matt", industry: "Sports" },
 ];
 
+//HOMEPAGE GET - returns Welcome Home on page
 app.get("/", (req, res) => res.send("Welcome Home!"));
-app.get("/api/contacts", (req, res) => res.send({"Peoples Data": people}));
+
+//GET REQ for CONTACTS - Returns all contacts
+app.get("/api/contacts", (req, res) => res.send({ "Peoples Data": people }));
+
+//HOMEPAGE POST - Dummy POST req
 app.post("/", (req, res) => res.send("This is POST requesssst"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+//POST REQ for individual contact
+app.post("/api/contacts", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(connectionString);
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () =>
+      console.log(`Example app listening on port ${PORT}!`)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+startServer()
