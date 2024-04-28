93545fa8c50ccfb3b3347400acbacf8fc0b71a79
Michael Salerno <msalerno1993@gmail.com>
2024-04-27 20:02:41 -0400

    use mock data to ensure endpoints work

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
-const {v4: uuidv4} = require('uuid')
+const express = require("express");
+const app = express();
+const port = 3000;
 
-console.log(uuidv4());
+const people = [
+  { name: "John", industry: "Tech" },
+  { name: "Jessica", industry: "Hairstylist" },
+  { name: "Matt", industry: "Sports" },
+];
+
+app.get("/", (req, res) => res.send("Welcome Home!"));
+app.get("/api/contacts", (req, res) => res.send({"Peoples Data": people}));
+app.post("/", (req, res) => res.send("This is POST requesssst"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
