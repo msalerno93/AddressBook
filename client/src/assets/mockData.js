d2caf868327d211d8876fb209551e80e0cd5a6b0
Michael Salerno <msalerno1993@gmail.com>
2024-05-02 18:39:37 -0400

    created design for add contact page

--- client/src/assets/mockData.js
+++ client/src/assets/mockData.js
@@ -48,3 +48,51 @@ export const contacts = [
       state: "NY",
     },
   ];
+
+  export const addContactData = [
+    {
+      id: 1,
+      name: "First Name",
+      input: "text"
+    },
+    {
+      id: 2,
+      name: "Last Name",
+      input: "text"
+    },
+    {
+      id: 3,
+      name: "Phone Number",
+      input: "text"
+    },
+    {
+      id: 4,
+      name: "Email",
+      input: "email"
+    },
+    {
+      id: 5,
+      name: "Street",
+      input: "text"
+    },
+    {
+      id: 6,
+      name: "City",
+      input: "text"
+    },
+    {
+      id: 7,
+      name: "State",
+      input: "text"
+    },
+    {
+      id: 8,
+      name: "Zip Code",
+      input: "text"
+    },
+    {
+      id: 9,
+      name: "Occupation",
+      input: "text"
+    },
+  ]
--- client/src/pages/addContact.jsx
+++ client/src/pages/addContact.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { addContactData } from "../assets/mockData";
+import {useNavigate} from "react-router-dom"
 
 const AddContact = () => {
-  return (
-    <div className='p-5'>This page allows you to add all information for a contact</div>
-  )
+
+const navigate = useNavigate()
+
+const postNewContact = () => {
+  navigate('/allcontacts')
 }
 
-export default AddContact
+  return (
+    <div className="p-10 text-center">
+      <div className="py-5 text-4xl font-bold">Add A New Contact!</div>
+      <form action="" method="POST" className="pb-10">
+        {addContactData.map(({ name, type, id }) => {
+          return (
+            <div key={id} className="grid grid-cols-2 lg:mx-[25%] md:mx-[25%] max-w-full">
+              <p className="pt-2 font-bold text-black text-xl">{name}:</p>{" "}
+              <input
+                type={type}
+                className="px-5 my-2 rounded-2xl bg-slate-400 text-black font-bold text-lg"
+              />
+            </div>
+          );
+        })}
+        {/* <p className='pt-2 font-bold text-black'>First Name:</p> <input type="text" className='px-5 my-2 rounded-2xl bg-slate-400 text-black font-bold text-md' />
+        <p className='pt-2 font-bold text-black'>First Name:</p> <input type="text" className='px-5 my-2 rounded-2xl bg-slate-400' />
+        <p className='pt-2 font-bold text-black'>First Name:</p> <input type="text" className='px-5 my-2 rounded-2xl bg-slate-400' />
+        <p className='pt-2 font-bold text-black'>First Name:</p> <input type="text" className='px-5 my-2 rounded-2xl bg-slate-400' />
+        <p className='pt-2 font-bold text-black'>First Name:</p> <input type="text" className='px-5 my-2 rounded-2xl bg-slate-400' />
+        <p className='pt-2 font-bold text-black'>First Name:</p> <input type="text" className='px-5 my-2 rounded-2xl bg-slate-400' /> */}
+      </form>
+      <button onClick={postNewContact} className="rounded-2xl text-xl font-bold mx-5 bg-purple-600 px-2 py-2 text-black hover:bg-purple-700">
+        Add Contact
+      </button>
+    </div>
+  );
+};
+
+export default AddContact;
